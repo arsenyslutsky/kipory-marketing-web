0e2a6c5375427d859cd76266ef255ed87fe0c7f7
Arseny Slutsky <arsenys@gmail.com>
2026-08-27 01:45:16 +0300

    fix: remove Vitest config require imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,4 @@
-const { fileURLToPath } = require('node:url');
-const { defineConfig } = require('vitest/config');
-
-module.exports = defineConfig({
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+module.exports = {
+  resolve: { alias: { '@': `${process.cwd()}/src` } },
   test: { environment: 'jsdom', setupFiles: ['./src/test/setup.ts'] },
-});
+};
